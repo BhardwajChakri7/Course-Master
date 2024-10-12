2f14fe23ba7af5e9a88d32de5379584b8f9e099b
BhardwajChakri7 <155712824+BhardwajChakri7@users.noreply.github.com>
2024-10-12 18:07:29 +0530

    Delete filemain.js

--- filemain.js
+++
@@ -1,4 +0,0 @@
-// JavaScript to toggle the nav-links when the burger menu is clicked
-document.querySelector('.burger').addEventListener('click', () => {
-    document.querySelector('.nav-links').classList.toggle('active');
-});
